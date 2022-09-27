ff081983e13e215b26631ebd72df88699fb67f92
chjack10 <christianyak@gmail.com>
2022-09-27 14:44:23 -0300

    component compound ready

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,7 +5,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { ShoppingPage } from '../02-components-patterns/pages/ShoppingPage';
 
 import logo from '../logo.svg';
 
@@ -17,37 +17,19 @@ export const Navigation = () => (
         <ul>
           <li>
             <NavLink
-              to='/lazy1'
+              to='/'
               className={({ isActive }) => (isActive ? 'nav-active' : '')}
             >
-              lazy1
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/lazy2'
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-            >
-              lazy2
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/lazy3'
-              className={({ isActive }) => (isActive ? 'nav-active' : '')}
-            >
-              lazy3
+              Shopping
             </NavLink>
           </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path='/lazy1' element={LazyPage1} />
-        <Route path='/lazy2' element={LazyPage2} />
-        <Route path='/lazy3' element={LazyPage3} />
+        <Route path='/' element={<ShoppingPage />} />
 
-        <Route path='/*' element={<Navigate to='/lazy1' replace />} />
+        <Route path='/*' element={<Navigate to='/' replace />} />
       </Routes>
     </div>
   </BrowserRouter>
