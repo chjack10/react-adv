f8f7cd0772a687aaca7d5d7a53e0c5cc942aa8b9
Christian <christianyak@gmail.com>
2022-10-24 11:34:21 -0300

    control props alt

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -14,38 +14,15 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-
-        return { ...oldShoppingCart, [product.id]: productInCart };
+      if (count === 0) {
+        delete oldShoppingCart[product.id];
+        return { ...oldShoppingCart };
       }
 
-      //delete product from shoppingCart
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      return rest;
-
-      // // if (count === 0) {
-      // //   delete oldShoppingCart[product.id];
-      // //  return  {...oldShoppingCart}
-      // // }
-
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
